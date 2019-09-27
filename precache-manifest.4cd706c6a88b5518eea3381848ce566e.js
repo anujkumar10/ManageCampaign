c82a1b0be7a8faf2236059e5ea2e844685ba1f8c
Anuj Kumar <anuj@Anuj2.local>
2019-09-27 05:57:23 -0400

    Updates

---
+++ precache-manifest.4cd706c6a88b5518eea3381848ce566e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d2d13f38d2a06f46ab57a96ca414e00",
+    "url": "/ManageCampaign/index.html"
+  },
+  {
+    "revision": "73fe1d51706b3bc0b366",
+    "url": "/ManageCampaign/static/css/2.e80e58db.chunk.css"
+  },
+  {
+    "revision": "3737a58f3ee1514ab2a1",
+    "url": "/ManageCampaign/static/css/main.63cbad5d.chunk.css"
+  },
+  {
+    "revision": "73fe1d51706b3bc0b366",
+    "url": "/ManageCampaign/static/js/2.de7ff585.chunk.js"
+  },
+  {
+    "revision": "3737a58f3ee1514ab2a1",
+    "url": "/ManageCampaign/static/js/main.3f7ec673.chunk.js"
+  },
+  {
+    "revision": "257ddff41ea822cc520b",
+    "url": "/ManageCampaign/static/js/runtime-main.8b46e8bc.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/ManageCampaign/static/media/logo.25bf045c.svg"
+  }
+]);
